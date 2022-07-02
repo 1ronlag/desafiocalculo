2fadb6e6625db86566121f9c4ac99139cc8c0c35
1ronlag <nico.saavedrap@gmail.com>
2022-07-02 01:05:40 -0400

    add circle and total

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,15 @@
 window.onload =function () {
 let boton= document.querySelector("#button");
 boton.addEventListener("click", ()=>{
-let cantidad = document.querySelector('#cantidad')
-let total = document.querySelector('#total')
-let unidad = document.getElementById('unidad')
-total.innerHTML = 400000*cantidad.value;
+let cantidad = document.querySelector('#cantidad');
+let selectorTotal = document.querySelector('#total');
+let totalApagar = 400000*cantidad.value;
+const totalformateado = new Intl.NumberFormat('es-CL', { style: 'currency', currency: 'CLP' }).format(totalApagar);
+selectorTotal.innerHTML = totalformateado;
+let unidad = document.getElementById('unidad');
 unidad.innerHTML = cantidad.value;
+let colorLaptop = document.querySelector ('#colorlaptop');
+let elementoCirculo = document.getElementById ('circleId')
+elementoCirculo.style.background = colorLaptop.value;
 })
-}
+}   
